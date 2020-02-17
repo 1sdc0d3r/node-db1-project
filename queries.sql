f7f8f303e3a9cebfb3e3b77f4a2cb544d19c4bfc
Braden <fish2355@hotmail.com>
2020-02-17 15:52:18 -0700

    changed to route, added knex queries to sql queries

--- queries.sql
+++ queries.sql
@@ -1,17 +1,63 @@
 -- Database Queries
-
--- Find all customers with postal code 1010
-SELECT * FROM Customers WHERE PostalCode = 1010
--- Find the phone number for the supplier with the id 11
-SELECT PhoneNumber FROM Customers WHERE id = 11
--- List first 10 orders placed, sorted descending by the order date
-SELECT * FROM Customers ORDER BY OrderDate DESC
--- Find all customers that live in London, Madrid, or Brazil
-SELECT * FROM Customers WHERE City = "London" OR City = "Madrid", OR Country = "Brazil"
--- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
-INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country) VALUES ("The Shire", "Bilbo Baggins", "1 Hobbit-Hole", "Bag End", 111, "Middle Earth")
--- Update Bilbo Baggins record so that the postal code changes to "11122"
-UPDATE Customers SET PostalCode = 11122 WHERE ContactName = "Bilbo Baggins"
--- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
-SELECT COUNT(DISTINCT City) FROM Customers
--- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+const db = require("./data/dbConfig.js") -- Find all customers with postal code 1010
+SELECT
+  *
+FROM Customers
+WHERE
+  PostalCode = 1010 db("Customers").where("PostalCode", "=", 1010) -- Find the phone number for the supplier with the id 11
+SELECT
+  Phone
+FROM Suppliers
+WHERE
+  SupplierId = 11 db("Suppliers").where("SupllierId", "=", 11).select("Phone") -- List first 10 orders placed, sorted descending by the order date
+SELECT
+  *
+FROM Orders
+ORDER BY
+  OrderDate
+LIMIT
+  10 db("Orders").orderBy("OrderDate", "desc").limit(10) -- Find all customers that live in London, Madrid, or Brazil
+SELECT
+  *
+FROM Customers
+WHERE
+  City = "London"
+  OR City = "Madrid",
+  OR Country = "Brazil" db("Customers").where("City", "London").orWhere("City", "Madrid").orWhere("Country", "Brazil") -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+INSERT INTO Customers (
+    CustomerName,
+    ContactName,
+    Address,
+    City,
+    PostalCode,
+    Country
+  )
+VALUES
+  (
+    "The Shire",
+    "Bilbo Baggins",
+    "1 Hobbit-Hole",
+    "Bag End",
+    111,
+    "Middle Earth"
+  ) db("Customers").insert(
+    { CustomerName : "The Shire",
+    ContactName : "Bilbo Baggins",
+    Address : "1 Hobbit-Hole",
+    City : "Bag End",
+    PostalCode : 111,
+    Country : "Middle Earth" }
+  ) -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE Customers
+SET
+  PostalCode = 11122
+WHERE
+  ContactName = "Bilbo Baggins" db("Customers").where("ContactName", "Bilbo Baggins").update("PostalCode", 11122) -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
+SELECT
+  COUNT(DISTINCT City)
+FROM Customers db("Customers").countDistinct("City") -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+select
+  *
+from Suppliers
+where
+  length(SupplierName) > 20 db("Suppliers").where(length("SupplierName", ">", 20))
--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -13,3 +13,55 @@ router.get("/", (req, res) => {
       })
     );
 });
+
+router.post("/", (req, res) => {
+  db("accounts")
+    .insert(req.body)
+    .then(account =>
+      res
+        .status(201)
+        .send({ response: account, message: "Account successfully added." })
+    )
+    .catch(err =>
+      res
+        .status(500)
+        .send({ error: err, message: "Account was not added to the database" })
+    );
+});
+
+// db('users').where({ id: 3 })
+// .update({name: 'Ava', age: 33 });
+
+router.put("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(account =>
+      res
+        .status(200)
+        .send({ result: account, message: "Account successfully updated" })
+    )
+    .catch(err => {
+      res
+        .status(500)
+        .send({ errorMessage: "Account was not updated", error: err });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .del()
+    .then(response =>
+      res
+        .status(200)
+        .send({ message: "Account successfully deleted", response: response })
+    )
+    .catch(err =>
+      res
+        .status(500)
+        .send({ errorMessage: "Account was not deleted", error: err })
+    );
+});
+
+module.exports = router;
